b9f4ad5287fe814576c4a27fbd7d0b77cdb1b3ba
Mayank Kar <mayank.kar2021@gmail.com>
2026-06-03 20:26:28 +0530

    Add: three-integer-sum - submission-1

---
+++ Data Structures & Algorithms/three-integer-sum/submission-1.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool same_idx(int a, int b, int c) {
+        if (a == b || a == c || b == c) {
+            return true;
+        }
+        return false;
+    }
+
+    vector<vector<int>> threeSum(vector<int>& nums) {        
+        set<pair<pair<int, int>, int>> s;
+        int n = nums.size(), target, l, r;
+        vector<vector<int>> ret;
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i<n; i++) {
+            target = -nums[i];
+            l = 0, r = n-1;
+            while (l < r) {
+                if (nums[l]+nums[r] < target) {
+                    l++;
+                } else if (nums[l]+nums[r] > target) {
+                    r--;
+                } else {
+                    if (!same_idx(i, l, r)) {
+                        vector<int> v = {nums[l], nums[r], -target};
+                        sort(v.begin(), v.end());
+                        s.insert({{v[0], v[1]}, v[2]});
+                    }
+                    l++; r--;
+                }
+            }
+        }
+        
+        for (auto three: s) {
+            vector<int> v = {three.first.first, three.first.second, three.second};
+            ret.push_back(v);
+        }
+        
+        return ret;
+    }
+};
